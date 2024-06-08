d36d0e79711157882b4106076307b5e75c3f58ec
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-09 01:24:37 +0530

    Update eslintrc file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,30 +3,6 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ['airbnb', 'airbnb/hooks'],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		ecmaFeatures: {
-			arrowFunctions: false,
-		},
-	},
-	rules: {
-		'react/react-in-jsx-scope': 'off',
-		indent: ['error', 'tab'],
-		'no-tabs': 'off',
-		'react/jsx-indent': ['error', 'tab'],
-		'react/function-component-definition': [
-			'error',
-			{
-				namedComponents: 'arrow-function',
-			},
-		],
-	},
-	env: {
-		browser: true,
-		es2021: true,
-	},
 	extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
 	parserOptions: {
 		ecmaVersion: 'latest',
