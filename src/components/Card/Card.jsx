090ec55b996430a903d31cd83b37c4ef251deb1d
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-10 17:54:17 +0530

    Add outlined card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,50 @@
+import {
+	Card as MUICard,
+	CardActions as MUICardActions,
+	CardContent as MUICardContent,
+} from '@mui/material';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+import Typography from '../Typography';
+
+const Card = ({ title, body, actions, ...props }) => {
+	let actionButtons;
+	if (actions) {
+		actionButtons = actions.map(({ url, label }) => {
+			return (
+				<Button
+					href={url}
+					variant="text"
+					target="_blank"
+					color="primary"
+					key={url}
+					sx={{
+						textTransform: 'uppercase',
+						fontSize: '1.4rem',
+					}}
+				>
+					{label}
+				</Button>
+			);
+		});
+	}
+	return (
+		<MUICard variant="outlined" {...props}>
+			<MUICardContent>
+				<Typography variant="h4" mb={3} gutterBottom>
+					{title}
+				</Typography>
+				<Typography variant="body1">{body}</Typography>
+			</MUICardContent>
+			{actions && <MUICardActions>{actionButtons}</MUICardActions>}
+		</MUICard>
+	);
+};
+
+Card.propTypes = {
+	title: PropTypes.string,
+	body: PropTypes.string,
+	actions: PropTypes.arrayOf(Object),
+};
+
+export default Card;
