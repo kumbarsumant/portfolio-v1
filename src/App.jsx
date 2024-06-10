42191f7c140e4f72e2ccd2df1d40f112916106c7
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-10 09:17:35 +0530

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import Button from './components/Button';
 import theme from './styles/js/theme';
 
 const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
-			<div>
-				<Button variant="contained" size="large">
-					Contact Me
-				</Button>
-				<Button variant="outlined" size="large">
-					Explore More
-				</Button>
-			</div>
+			<App>App</App>
 		</ThemeProvider>
 	);
 };
