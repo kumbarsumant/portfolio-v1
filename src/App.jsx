fce48cba8276d92ea804acf42c402949d8262e48
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-10 13:29:36 +0530

    update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/js/theme';
 
 const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<App>App</App>
-		</ThemeProvider>
-	);
+	return <ThemeProvider theme={theme}>App</ThemeProvider>;
 };
 
 export default App;
