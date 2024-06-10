692440c2ccde2508726f85a381e7863f448edf3d
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-10 13:19:26 +0530

    Merge components folder from documents branch to main

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,34 @@
+import { Button as MUIButton } from '@mui/material';
+import PropTypes from 'prop-types';
+import { styleVariables } from '../../styles';
+
+const { fonts } = styleVariables;
+
+const Button = ({
+	children,
+	variant = 'contained',
+	color = 'primary',
+	...props
+}) => {
+	const customStyles = {
+		borderRadius: 0,
+		boxShadow: 'none',
+		textTransform: 'capitalize',
+		fontFamily: fonts.tertiaryFont,
+		fontWeight: 400,
+		fontSize: '1.6rem',
+	};
+
+	return (
+		<MUIButton variant={variant} color={color} sx={customStyles} {...props}>
+			{children}
+		</MUIButton>
+	);
+};
+Button.propTypes = {
+	children: PropTypes.node,
+	variant: PropTypes.oneOf(['text', 'outlined', 'contained']),
+	color: PropTypes.oneOf(['primary', 'secondary']),
+};
+
+export default Button;
---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { TextField as MUITextField } from '@mui/material';
+import { styleVariables } from '../../styles';
+
+const { fonts } = styleVariables;
+
+const TextField = ({ label = '', variant = 'standard', ...props }) => {
+	return (
+		<MUITextField
+			variant={variant}
+			label={label}
+			sx={{
+				'& .MuiInput-input': {
+					fontSize: '1.6rem',
+				},
+				'& .MuiInputLabel-root': {
+					fontSize: '1.6rem',
+					fontFamily: fonts.tertiaryFont,
+				},
+				'& .MuiFormHelperText-root': {
+					fontFamily: fonts.tertiaryFont,
+					fontSize: '1.2rem',
+				},
+			}}
+			{...props}
+		/>
+	);
+};
+
+TextField.propTypes = {
+	label: PropTypes.string,
+	variant: PropTypes.oneOf(['standard', 'outlined', 'filled']),
+};
+
+export default TextField;
