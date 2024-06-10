071b16a369619325abd42104d9bd607129afb34c
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-06-11 00:13:24 +0530

    Add menu item component

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import Link from '../Link';
+import styles from './MenuItem.module.scss';
+
+const MenuItem = ({ href, children, active }) => {
+	const classes = active ? styles.active : styles.normal;
+	return (
+		<li className={classes}>
+			<Link href={href} variant={active ? 'primary' : 'info'}>
+				{children}
+			</Link>
+		</li>
+	);
+};
+
+MenuItem.propTypes = {
+	href: PropTypes.string,
+	children: PropTypes.node,
+	active: PropTypes.bool,
+};
+
+export default MenuItem;
